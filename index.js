e03f60d8fd8c8f122ba3d596809adb1c2fe0d708
Pasha <pashdoctor@gmail.com>
2021-09-04 12:24:36 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+
+let random = Math.floor(Math.random() * 6) + 1
+
+const num3 = 8992;
+const num4 = 42;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3 ,4, 20);
